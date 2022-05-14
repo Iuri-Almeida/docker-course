2c9f7d95bd1a905913f2bbc198dd9b9006af0855
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-05-14 00:52:06 -0300

    Networks - Host

---
+++ networks/host/app.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask_mysqldb import MySQL
+from requests import get
+
+app = Flask(__name__)
+
+app.config["DEBUG"] = True
+app.config["MYSQL_HOST"] = "host.docker.internal"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = ""
+app.config["MYSQL_DB"] = "flaskhost"
+
+mysql = MySQL(app)
+
+@app.route("/", methods=["GET"])
+def index():
+    data = get("https://randomuser.me/api")
+    return data.json()
+
+
+@app.route("/inserthost", methods=["POST"])
+def inser_thost():
+    data = get("https://randomuser.me/api").json()
+    user_name = data["results"][0]["name"]["first"]
+
+    cur = mysql.connection.cursor()
+    cur.execute("""INSERT INTO users(name) VALUES(%s)""", (user_name))
+    mysql.connection.commit()
+    cur.close()
+
+    return user_name
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
